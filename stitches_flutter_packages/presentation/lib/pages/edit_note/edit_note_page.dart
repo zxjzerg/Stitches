9df7d7fb3ae016bf404061489a1f7da5bdb4307f
Andrew Zhao <zxjzerg@outlook.com>
2023-05-24 15:34:41 +0800

    引入Flutter Hooks
    把编辑笔记界面改为HookWidget
    笔记列表界面改为StatelessWidget

--- stitches_flutter_packages/presentation/lib/pages/edit_note/edit_note_page.dart
+++ stitches_flutter_packages/presentation/lib/pages/edit_note/edit_note_page.dart
@@ -1,107 +1,93 @@
 import 'package:core/entity/note.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:presentation/pages/edit_note/bloc/edit_note_bloc.dart';
 
 /// 创建/编辑笔记界面
-class EditNotePage extends StatefulWidget {
+class EditNotePage extends HookWidget {
   const EditNotePage({Key? key}) : super(key: key);
 
   @override
-  State<EditNotePage> createState() => _EditNotePageState();
-}
-
-class _EditNotePageState extends State<EditNotePage> {
-  final _titleTextController = TextEditingController();
-  final _contentTextController = TextEditingController();
-
-  @override
-  void dispose() {
-    super.dispose();
-    _titleTextController.dispose();
-    _contentTextController.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    EditNoteBloc? bloc;
+    final titleTextController = useTextEditingController();
+    final contentTextController = useTextEditingController();
     final Note? note = ModalRoute.of(context)!.settings.arguments as Note?;
     if (note?.title?.isNotEmpty == true) {
-      _titleTextController.text = note!.title!;
+      titleTextController.text = note!.title!;
     }
     if (note?.content?.isNotEmpty == true) {
-      _contentTextController.text = note!.content!;
+      contentTextController.text = note!.content!;
     }
     return BlocProvider<EditNoteBloc>(
-      create: (context) {
-        bloc = EditNoteBloc();
-        return bloc!;
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("编辑笔记"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.done),
-              onPressed: () {
-                bloc?.add(
-                  EditNoteEvent(
-                    Note(
-                      id: note?.id,
-                      content: _contentTextController.text,
-                      title: _titleTextController.text,
+      create: (context) => EditNoteBloc(),
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("编辑笔记"),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.done),
+                onPressed: () {
+                  BlocProvider.of<EditNoteBloc>(context).add(
+                    EditNoteEvent(
+                      Note(
+                        id: note?.id,
+                        content: titleTextController.text,
+                        title: contentTextController.text,
+                      ),
                     ),
-                  ),
-                );
-              },
-            ),
-          ],
-        ),
-        body: BlocListener<EditNoteBloc, EditNoteState>(
-          listener: (context, state) {
-            debugPrint(state.toString());
-            if (state is EditNoteSuccess) {
-              Navigator.pop(context, true);
-            }
-          },
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 12, left: 16, right: 16),
-                child: TextField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: "标题",
-                    contentPadding: EdgeInsets.all(12),
-                  ),
-                  controller: _titleTextController,
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    top: 12,
-                    left: 16,
-                    right: 16,
-                    bottom: 12,
-                  ),
-                  child: TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '内容',
-                      contentPadding: EdgeInsets.all(12),
-                    ),
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    minLines: 100,
-                    controller: _contentTextController,
-                  ),
-                ),
+                  );
+                },
               ),
             ],
           ),
-        ),
-      ),
+          body: BlocListener<EditNoteBloc, EditNoteState>(
+            listener: (context, state) {
+              debugPrint(state.toString());
+              if (state is EditNoteSuccess) {
+                Navigator.pop(context, true);
+              }
+            },
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 12, left: 16, right: 16),
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "标题",
+                      contentPadding: EdgeInsets.all(12),
+                    ),
+                    controller: titleTextController,
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 12,
+                      left: 16,
+                      right: 16,
+                      bottom: 12,
+                    ),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '内容',
+                        contentPadding: EdgeInsets.all(12),
+                      ),
+                      keyboardType: TextInputType.multiline,
+                      maxLines: null,
+                      minLines: 100,
+                      controller: contentTextController,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
--- stitches_flutter_packages/presentation/lib/pages/note_list/note_list_page.dart
+++ stitches_flutter_packages/presentation/lib/pages/note_list/note_list_page.dart
@@ -5,78 +5,64 @@ import 'package:presentation/common/stitches_route.dart';
 import 'package:presentation/pages/note_list/bloc/note_list_bloc.dart';
 
 /// 笔记列表界面
-class NoteListPage extends StatefulWidget {
+class NoteListPage extends StatelessWidget {
   const NoteListPage({Key? key}) : super(key: key);
-
-  @override
-  State<NoteListPage> createState() => _NoteListPageState();
-}
-
-class _NoteListPageState extends State<NoteListPage> {
-  late NoteListBloc _noteListBloc;
-
-  @override
-  void initState() {
-    _noteListBloc = NoteListBloc();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _noteListBloc.close();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider<NoteListBloc>(
-      create: (context) => _noteListBloc..add(LoadNoteListEvent()),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("笔记列表"),
-          actions: [
-            // 删除所有笔记
-            IconButton(
-              icon: const Icon(Icons.delete),
-              tooltip: 'delete all notes',
-              onPressed: () => _noteListBloc.add(DeleteAllNotesEvent()),
-            ),
-          ],
-        ),
-        body: BlocBuilder<NoteListBloc, NoteListState>(
-          builder: (context, state) {
-            return ListView.builder(
-              itemBuilder: (context, index) {
-                final Note note = state.notes[index];
-                return ListTile(
-                  title: Text("${note.title}"),
-                  subtitle: Text("${note.content}"),
-                  onTap: () => Navigator.pushNamed(
-                    context,
-                    StitchesRoute.editNotePage,
-                    arguments: note,
-                  ),
-                );
-              },
-              itemCount: state.notes.length,
-            );
-          },
-        ),
-        // 创建笔记
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            Navigator.pushNamed(context, StitchesRoute.editNotePage).then(
-              (value) {
-                // 检查返回值，如果返回值是改动了笔记，则需要刷新笔记列表
-                if (value != null) {
-                  if (value as bool) _noteListBloc.add(LoadNoteListEvent());
-                }
-              },
-            );
-          },
-          child: const Icon(Icons.add),
-        ),
-      ),
+      create: (context) => NoteListBloc()..add(LoadNoteListEvent()),
+      child: Builder(builder: (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("笔记列表"),
+            actions: [
+              // 删除所有笔记
+              IconButton(
+                icon: const Icon(Icons.delete),
+                tooltip: 'delete all notes',
+                onPressed: () => BlocProvider.of<NoteListBloc>(context)
+                    .add(DeleteAllNotesEvent()),
+              ),
+            ],
+          ),
+          body: BlocBuilder<NoteListBloc, NoteListState>(
+            builder: (context, state) {
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Note note = state.notes[index];
+                  return ListTile(
+                    title: Text("${note.title}"),
+                    subtitle: Text("${note.content}"),
+                    onTap: () => Navigator.pushNamed(
+                      context,
+                      StitchesRoute.editNotePage,
+                      arguments: note,
+                    ),
+                  );
+                },
+                itemCount: state.notes.length,
+              );
+            },
+          ),
+          // 创建笔记
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.pushNamed(context, StitchesRoute.editNotePage).then(
+                (value) {
+                  // 检查返回值，如果返回值是改动了笔记，则需要刷新笔记列表
+                  if (value != null) {
+                    if (value as bool)
+                      BlocProvider.of<NoteListBloc>(context)
+                          .add(LoadNoteListEvent());
+                  }
+                },
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
+        );
+      }),
     );
   }
 }
