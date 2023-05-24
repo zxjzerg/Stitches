cdf498e456b74fb5ecc97cfac11c81dc1e69495c
Andrew Zhao <zxjzerg@outlook.com>
2023-05-24 16:44:53 +0800

    进入了Jetpack Compose并且修改主界面使用Compose

--- stitches_android/app/src/main/java/io/github/zxjzerg/stitches/MainActivity.kt
+++ stitches_android/app/src/main/java/io/github/zxjzerg/stitches/MainActivity.kt
@@ -1,24 +1,37 @@
 package io.github.zxjzerg.stitches
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import io.github.zxjzerg.flutter.StitchesFlutterEngineGroupActivity
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnStartFlutterEngineGroupActivity: Button
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        btnStartFlutterEngineGroupActivity = findViewById(R.id.btnStartFlutterEngineGroupActivity)
-        btnStartFlutterEngineGroupActivity.setOnClickListener {
-            val intent = Intent(this, StitchesFlutterEngineGroupActivity::class.java)
-            startActivity(intent)
+        setContent {
+            MainPage()
+        }
+    }
+
+    @Composable
+    @Preview
+    fun MainPage() {
+        Column {
+            Button(onClick = {
+                val intent = Intent(this@MainActivity, StitchesFlutterEngineGroupActivity::class.java)
+                startActivity(intent)
+            }) {
+                Text("启动笔记界面（Flutter）")
+            }
         }
     }
 }
